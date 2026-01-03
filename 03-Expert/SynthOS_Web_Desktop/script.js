66230d3300d45ce11714d04797bf99d4a0cdd908
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2026-01-03 17:10:32 +0530

    chore(copyright): update headers and license information © 2026

--- 03-Expert/SynthOS_Web_Desktop/script.js
+++ 03-Expert/SynthOS_Web_Desktop/script.js
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 /**
  * Project: SynthOS_Web_Desktop
  * Author: Ashraf Morningstar
